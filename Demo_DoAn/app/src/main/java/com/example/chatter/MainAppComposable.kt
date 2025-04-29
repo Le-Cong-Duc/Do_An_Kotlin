2e0d922cddb452817135ba8f074c063940c09182
Le-Cong-Duc <congduc1801@gmail.com>
2025-04-29 21:35:10 +0700

    Demo Do An

---
+++ Demo_DoAn/app/src/main/java/com/example/chatter/MainAppComposable.kt
@@ -0,0 +1,41 @@
+package com.example.chatter
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.chatter.feature.auth.signin.SignInScreen
+import com.example.chatter.feature.auth.signup.SignUpScreen
+import com.example.chatter.feature.home.HomeScreen
+import com.google.firebase.auth.FirebaseAuth
+
+@Composable
+fun MainApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val navController = rememberNavController()
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val checkLogin = if (currentUser != null) "home" else "login"
+
+        NavHost(
+            navController = navController,
+            startDestination = checkLogin
+        ) {
+            composable("login") {
+                SignInScreen(navController)
+            }
+            composable("signup") {
+                SignUpScreen(navController)
+            }
+            composable("home") {
+                HomeScreen(navController)
+            }
+        }
+
+    }
+}
